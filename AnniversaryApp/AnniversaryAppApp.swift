0830ef3dda0f8d1e7778df2fcce37c44ea6916c0
higurashitaishi <tanishi0320@icloud.com>
2026-05-29 13:28:55 +0900

    Initial Commit

---
+++ AnniversaryApp/AnniversaryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnniversaryAppApp.swift
+//  AnniversaryApp
+//
+//  Created by higurashi on 2026/05/29.
+//
+
+import SwiftUI
+
+@main
+struct AnniversaryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
